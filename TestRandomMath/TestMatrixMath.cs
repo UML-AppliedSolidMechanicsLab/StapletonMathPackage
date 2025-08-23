c64a94df4837033c7b701b9b1ae27c6e4a06a1d3
sstaple <scott_stapleton@uml.edu>
2025-08-23 12:42:10 -0400

    Minor changes...

--- TestRandomMath/TestMatrixMath.cs
+++ TestRandomMath/TestMatrixMath.cs
@@ -36,9 +36,9 @@ namespace TestRandomMath
             myB[2, 0] = 7.0;
             MatrixMath.Gauss(myA, myB, myn, myC);
             double acceptablePrecision = 0.000000000001;
-            Assert.AreEqual(-6.44444444444444, myB[0, 0], acceptablePrecision);
-            Assert.AreEqual(-1.66666666666667, myB[1, 0], acceptablePrecision);
-            Assert.AreEqual(9.88888888888889, myB[2, 0], acceptablePrecision);
+            Assert.That(myB[0, 0], Is.EqualTo(-6.44444444444444).Within(acceptablePrecision));
+            Assert.That(myB[1, 0], Is.EqualTo(-1.66666666666667).Within(acceptablePrecision));
+            Assert.That(myB[2, 0], Is.EqualTo(9.88888888888889).Within(acceptablePrecision));
         }
         [Test]
         public void testMatrixMultiplication()
@@ -61,10 +61,10 @@ namespace TestRandomMath
 
             myC = MatrixMath.Multiply(myA, myB);
 
-            Assert.AreEqual(0.25, myC[0, 0], acceptablePrecision);
-            Assert.AreEqual(0.5, myC[0, 1], acceptablePrecision);
-            Assert.AreEqual(0.4375, myC[1, 0], acceptablePrecision);
-            Assert.AreEqual(0.8125, myC[1, 1], acceptablePrecision);
+            Assert.That(myC[0, 0], Is.EqualTo(0.25).Within(acceptablePrecision));
+            Assert.That(myC[0, 1], Is.EqualTo(0.5).Within(acceptablePrecision));
+            Assert.That(myC[1, 0], Is.EqualTo(0.4375).Within(acceptablePrecision));
+            Assert.That(myC[1, 1], Is.EqualTo(0.8125).Within(acceptablePrecision));
         }
         [Test]
         public void testInvertMatrix()
@@ -85,15 +85,15 @@ namespace TestRandomMath
 
             myC = MatrixMath.InvertMatrix(myA);
 
-            Assert.AreEqual(-0.32, myC[0, 0], acceptablePrecision);
-            Assert.AreEqual(0.52, myC[0, 1], acceptablePrecision);
-            Assert.AreEqual(-0.48, myC[0, 2], acceptablePrecision);
-            Assert.AreEqual(-0.48, myC[1, 0], acceptablePrecision);
-            Assert.AreEqual(-0.72, myC[1, 1], acceptablePrecision);
-            Assert.AreEqual(1.28, myC[1, 2], acceptablePrecision);
-            Assert.AreEqual(0.64, myC[2, 0], acceptablePrecision);
-            Assert.AreEqual(-0.04, myC[2, 1], acceptablePrecision);
-            Assert.AreEqual(-0.04, myC[2, 2], acceptablePrecision);
+            Assert.That(myC[0, 0], Is.EqualTo(-0.32).Within(acceptablePrecision));
+            Assert.That(myC[0, 1], Is.EqualTo(0.52).Within(acceptablePrecision));
+            Assert.That(myC[0, 2], Is.EqualTo(-0.48).Within(acceptablePrecision));
+            Assert.That(myC[1, 0], Is.EqualTo(-0.48).Within(acceptablePrecision));
+            Assert.That(myC[1, 1], Is.EqualTo(-0.72).Within(acceptablePrecision));
+            Assert.That(myC[1, 2], Is.EqualTo(1.28).Within(acceptablePrecision));
+            Assert.That(myC[2, 0], Is.EqualTo(0.64).Within(acceptablePrecision));
+            Assert.That(myC[2, 1], Is.EqualTo(-0.04).Within(acceptablePrecision));
+            Assert.That(myC[2, 2], Is.EqualTo(-0.04).Within(acceptablePrecision));
         }
         [Test]
         public void testSymmetricBandedSolver()
@@ -103,11 +103,11 @@ namespace TestRandomMath
             double[] myC = MatrixMath.SymmetricBandedSolver(myA, myB);
             double acceptablePrecision = 1.0E-9;
 
-            Assert.AreEqual(14.079754601226993865, myC[0], acceptablePrecision);
-            Assert.AreEqual(-6.5398773006134969325, myC[1], acceptablePrecision);
-            Assert.AreEqual(-2.1349693251533742331, myC[2], acceptablePrecision);
-            Assert.AreEqual(65.779141104294478528, myC[3], acceptablePrecision);
-            Assert.AreEqual(-26.711656441717791411, myC[4], acceptablePrecision);
+            Assert.That(myC[0], Is.EqualTo(14.079754601226993865).Within(acceptablePrecision));
+            Assert.That(myC[1], Is.EqualTo(-6.5398773006134969325).Within(acceptablePrecision));
+            Assert.That(myC[2], Is.EqualTo(-2.1349693251533742331).Within(acceptablePrecision));
+            Assert.That(myC[3], Is.EqualTo(65.779141104294478528).Within(acceptablePrecision));
+            Assert.That(myC[4], Is.EqualTo(-26.711656441717791411).Within(acceptablePrecision));
 
             //Try another test, a little bit hairier!!
 
@@ -116,14 +116,14 @@ namespace TestRandomMath
             double[] myB1 = new double[8] { 6, 7, 8, 9, 0, 2, 3, 4 };
             double[] myC1 = MatrixMath.SymmetricBandedSolver(myA1, myB1);
 
-            Assert.AreEqual(3.6792959471903019873, myC1[0], acceptablePrecision);
-            Assert.AreEqual(-.11799136471442218435, myC1[1], acceptablePrecision);
-            Assert.AreEqual(.58407892062480899170, myC1[2], acceptablePrecision);
-            Assert.AreEqual(-1.1730247046207359954, myC1[3], acceptablePrecision);
-            Assert.AreEqual(.11905421305965486397, myC1[4], acceptablePrecision);
-            Assert.AreEqual(-1.6408479084195896295, myC1[5], acceptablePrecision);
-            Assert.AreEqual(.59635467612136649060, myC1[6], acceptablePrecision);
-            Assert.AreEqual(.29379330513793170041, myC1[7], acceptablePrecision);
+            Assert.That(myC1[0], Is.EqualTo(3.6792959471903019873).Within(acceptablePrecision));
+            Assert.That(myC1[1], Is.EqualTo(-.11799136471442218435).Within(acceptablePrecision));
+            Assert.That(myC1[2], Is.EqualTo(.58407892062480899170).Within(acceptablePrecision));
+            Assert.That(myC1[3], Is.EqualTo(-1.1730247046207359954).Within(acceptablePrecision));
+            Assert.That(myC1[4], Is.EqualTo(.11905421305965486397).Within(acceptablePrecision));
+            Assert.That(myC1[5], Is.EqualTo(-1.6408479084195896295).Within(acceptablePrecision));
+            Assert.That(myC1[6], Is.EqualTo(.59635467612136649060).Within(acceptablePrecision));
+            Assert.That(myC1[7], Is.EqualTo(.29379330513793170041).Within(acceptablePrecision));
 
         }
         [Test]
@@ -145,47 +145,47 @@ namespace TestRandomMath
 
             double[,] myC = MatrixMath.SwapRows(myA, 1, 2);
 
-            Assert.AreEqual(1, myC[0, 0], acceptablePrecision);
-            Assert.AreEqual(2, myC[0, 1], acceptablePrecision);
-            Assert.AreEqual(9, myC[1, 0], acceptablePrecision);
-            Assert.AreEqual(10, myC[1, 1], acceptablePrecision);
-            Assert.AreEqual(5, myC[2, 0], acceptablePrecision);
-            Assert.AreEqual(6, myC[2, 1], acceptablePrecision);
-            Assert.AreEqual(13, myC[3, 0], acceptablePrecision);
-            Assert.AreEqual(14, myC[3, 1], acceptablePrecision);
+            Assert.That(myC[0, 0], Is.EqualTo(1).Within(acceptablePrecision));
+            Assert.That(myC[0, 1], Is.EqualTo(2).Within(acceptablePrecision));
+            Assert.That(myC[1, 0], Is.EqualTo(9).Within(acceptablePrecision));
+            Assert.That(myC[1, 1], Is.EqualTo(10).Within(acceptablePrecision));
+            Assert.That(myC[2, 0], Is.EqualTo(5).Within(acceptablePrecision));
+            Assert.That(myC[2, 1], Is.EqualTo(6).Within(acceptablePrecision));
+            Assert.That(myC[3, 0], Is.EqualTo(13).Within(acceptablePrecision));
+            Assert.That(myC[3, 1], Is.EqualTo(14).Within(acceptablePrecision));
 
             myC = MatrixMath.SwapCols(myC, 1, 2);
 
-            Assert.AreEqual(1, myC[0, 0], acceptablePrecision);
-            Assert.AreEqual(2, myC[0, 2], acceptablePrecision);
-            Assert.AreEqual(9, myC[1, 0], acceptablePrecision);
-            Assert.AreEqual(10, myC[1, 2], acceptablePrecision);
-            Assert.AreEqual(5, myC[2, 0], acceptablePrecision);
-            Assert.AreEqual(6, myC[2, 2], acceptablePrecision);
-            Assert.AreEqual(13, myC[3, 0], acceptablePrecision);
-            Assert.AreEqual(14, myC[3, 2], acceptablePrecision);
+            Assert.That(myC[0, 0], Is.EqualTo(1).Within(acceptablePrecision));
+            Assert.That(myC[0, 2], Is.EqualTo(2).Within(acceptablePrecision));
+            Assert.That(myC[1, 0], Is.EqualTo(9).Within(acceptablePrecision));
+            Assert.That(myC[1, 2], Is.EqualTo(10).Within(acceptablePrecision));
+            Assert.That(myC[2, 0], Is.EqualTo(5).Within(acceptablePrecision));
+            Assert.That(myC[2, 2], Is.EqualTo(6).Within(acceptablePrecision));
+            Assert.That(myC[3, 0], Is.EqualTo(13).Within(acceptablePrecision));
+            Assert.That(myC[3, 2], Is.EqualTo(14).Within(acceptablePrecision));
 
             myC = MatrixMath.SwapRows(myA, 0, 0, 2, 3);
 
-            Assert.AreEqual(9, myC[0, 0], acceptablePrecision);
-            Assert.AreEqual(10, myC[0, 1], acceptablePrecision);
-            Assert.AreEqual(13, myC[1, 0], acceptablePrecision);
-            Assert.AreEqual(14, myC[1, 1], acceptablePrecision);
-            Assert.AreEqual(5, myC[2, 0], acceptablePrecision);
-            Assert.AreEqual(6, myC[2, 1], acceptablePrecision);
-            Assert.AreEqual(1, myC[3, 0], acceptablePrecision);
-            Assert.AreEqual(2, myC[3, 1], acceptablePrecision);
+            Assert.That(myC[0, 0], Is.EqualTo(9).Within(acceptablePrecision));
+            Assert.That(myC[0, 1], Is.EqualTo(10).Within(acceptablePrecision));
+            Assert.That(myC[1, 0], Is.EqualTo(13).Within(acceptablePrecision));
+            Assert.That(myC[1, 1], Is.EqualTo(14).Within(acceptablePrecision));
+            Assert.That(myC[2, 0], Is.EqualTo(5).Within(acceptablePrecision));
+            Assert.That(myC[2, 1], Is.EqualTo(6).Within(acceptablePrecision));
+            Assert.That(myC[3, 0], Is.EqualTo(1).Within(acceptablePrecision));
+            Assert.That(myC[3, 1], Is.EqualTo(2).Within(acceptablePrecision));
 
             myC = MatrixMath.SwapCols(myA, 1, 1, 2, 3);
 
-            Assert.AreEqual(1, myC[0, 0], acceptablePrecision);
-            Assert.AreEqual(3, myC[0, 1], acceptablePrecision);
-            Assert.AreEqual(4, myC[0, 2], acceptablePrecision);
-            Assert.AreEqual(2, myC[0, 3], acceptablePrecision);
-            Assert.AreEqual(5, myC[1, 0], acceptablePrecision);
-            Assert.AreEqual(7, myC[1, 1], acceptablePrecision);
-            Assert.AreEqual(8, myC[1, 2], acceptablePrecision);
-            Assert.AreEqual(6, myC[1, 3], acceptablePrecision);
+            Assert.That(myC[0, 0], Is.EqualTo(1).Within(acceptablePrecision));
+            Assert.That(myC[0, 1], Is.EqualTo(3).Within(acceptablePrecision));
+            Assert.That(myC[0, 2], Is.EqualTo(4).Within(acceptablePrecision));
+            Assert.That(myC[0, 3], Is.EqualTo(2).Within(acceptablePrecision));
+            Assert.That(myC[1, 0], Is.EqualTo(5).Within(acceptablePrecision));
+            Assert.That(myC[1, 1], Is.EqualTo(7).Within(acceptablePrecision));
+            Assert.That(myC[1, 2], Is.EqualTo(8).Within(acceptablePrecision));
+            Assert.That(myC[1, 3], Is.EqualTo(6).Within(acceptablePrecision));
         }
     }
 }
